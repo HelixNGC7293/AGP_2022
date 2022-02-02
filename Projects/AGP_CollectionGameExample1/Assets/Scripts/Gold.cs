28a56c6cad0372499fc8fe9db5a625fcd9d1b37a
DGSpitzer <whu1@sva.edu>
2022-02-02 04:46:51 -0500

    AI Logic

---
+++ Projects/AGP_CollectionGameExample1/Assets/Scripts/Gold.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Gold : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other)
+    {
+        if (!other.tag.Contains("GameAgent")) return;
+
+        var teamID = int.Parse(other.tag.Split("_")[1]);
+        Services.EventManager.Fire(new Event_GoalScored(teamID));
+        Services.AIManager.DestroyGold(this);
+    }
+}
