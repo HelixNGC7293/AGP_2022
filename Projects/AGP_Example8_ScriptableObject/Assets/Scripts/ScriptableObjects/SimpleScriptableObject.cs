1df872b65bdf3f21e23ff9f761ade5bd6e4338f3
DGSpitzer <whu1@sva.edu>
2022-04-06 13:03:19 -0400

    Example 8

---
+++ Projects/AGP_Example8_ScriptableObject/Assets/Scripts/ScriptableObjects/SimpleScriptableObject.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum GameLevelType {Menu, InGame, EndScreen}
+
+[CreateAssetMenu(menuName = "Asset/Example Scriptable Object")]
+public class SimpleScriptableObject : ScriptableObject
+{
+	public float timeAllowed;
+	public GameObject[] levelMonsters;
+	public GameObject[] levelItems;
+	public GameLevelType gameLevelType;
+}
