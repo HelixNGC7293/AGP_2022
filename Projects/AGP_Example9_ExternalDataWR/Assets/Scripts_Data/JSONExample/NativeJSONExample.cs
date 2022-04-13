25c43fb60d535f4b9843a5ac1720e976e67875b9
DGSpitzer <whu1@sva.edu>
2022-04-13 03:19:04 -0400

    NativeJSONExample

--- Projects/AGP_Example9_ExternalDataWR/Assets/Scripts_Data/JSONExample/NativeJSONExample.cs
+++ Projects/AGP_Example9_ExternalDataWR/Assets/Scripts_Data/JSONExample/NativeJSONExample.cs
@@ -11,6 +11,7 @@ public class PlayerInfo
     public float health;
     public Skills skills;
 
+    [System.Serializable]
     public struct Skills
 	{
         public string skill1ID;
@@ -18,14 +19,15 @@ public class PlayerInfo
         public string skill3ID;
     }
 
-    public static PlayerInfo CreateFromJSON(string jsonString)
+    public static PlayerInfo CreateFromJSON(string path)
     {
+        string jsonString = File.ReadAllText(Application.streamingAssetsPath + path);
         return JsonUtility.FromJson<PlayerInfo>(jsonString);
     }
     public static void WriteToJSON(string path, PlayerInfo playerInfo)
     {
-        string content = JsonUtility.ToJson(playerInfo);
-        File.WriteAllText(Application.streamingAssetsPath + path, content);
+        string jsonString = JsonUtility.ToJson(playerInfo);
+        File.WriteAllText(Application.streamingAssetsPath + path, jsonString);
     }
 }
 
@@ -34,15 +36,23 @@ public class NativeJSONExample : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        //Save
         PlayerInfo newPlayer = new PlayerInfo();
         newPlayer.name = "Bob";
         newPlayer.lives = 10;
         newPlayer.health = 9999;
-        newPlayer.skills.skill1ID = "Fireball";
-        newPlayer.skills.skill2ID = "Fireball";
-        newPlayer.skills.skill3ID = "Fireball";
+        newPlayer.skills.skill1ID = "FireBall";
+        newPlayer.skills.skill2ID = "IceBall";
+        newPlayer.skills.skill3ID = "DragonBall";
 
         PlayerInfo.WriteToJSON("/JSON/Playerinfo.json", newPlayer);
+
+
+
+        //Create
+        PlayerInfo newPlayer2 = PlayerInfo.CreateFromJSON("/JSON/Playerinfo2.json");
+
+        Debug.Log(newPlayer2.name);
     }
 
     // Update is called once per frame
